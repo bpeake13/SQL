7ac70dcd90e2cd7b6f96118f9227d8b8413bced3
Jonathan Leitschuh <Jlleitschuh@wpi.edu>
2016-02-17 16:34:20 -0500

    Update population.sql

--- population.sql
+++ population.sql
@@ -2,4 +2,17 @@ INSERT INTO Tour (tourName, description, city, state, duration, vehicleType, adu
   VALUES ('Tour A', 'A really, really fun tour', 'Burlington', 'Vermont', 2, 'car', 39, 20);
 
 INSERT INTO Tour (tourName, description, city, state, duration, vehicleType, adultCost, childCost)
-  VALUES ('Tour B', 'A really, really, really fun tour', 'Burlington', 'Massachusets', 2, 'car', 39, 20);
+  VALUES ('Tour B', 'A really, really, really fun tour', 'Burlington', 'Massachusets', 4, 'amphibious', 50, 23);
+
+INSERT INTO Tour (tourName, description, city, state, duration, vehicleType, adultCost, childCost)
+  VALUES ('Tour C', 'A really, really, really, really fun tour', 'Worester', 'Massachusets', 3, 'bus', 49, 21);
+
+INSERT INTO Tour (tourName, description, city, state, duration, vehicleType, adultCost, childCost)
+  VALUES ('Tour D', 'A not so fun tour', 'White City', 'Massachusets', 40, 'bus', 499, 300);
+
+INSERT INTO Tour (tourName, description, city, state, duration, vehicleType, adultCost, childCost)
+  VALUES ('Tour E', 'The tour of a life time, without the Disney', 'Orlando', 'Florida', 2, 'amphibious', 10.23, 1.99);
+
+INSERT INTO Tour (tourName, description, city, state, duration, vehicleType, adultCost, childCost)
+  VALUES ('Tour F', 'Why are you going on this tour?', 'Forks', 'Washington', 99, 'bus', 432, 3.11);
+
