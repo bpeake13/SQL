daaf4fcce374b20ee5a393548a2a4f37020a8bd3
bpeake13 <bennypeake@gmail.com>
2016-02-17 16:16:48 -0500

    Made some edits

--- tables.sql
+++ tables.sql
@@ -13,13 +13,15 @@ CREATE TABLE Guide (
 );
 
 CREATE TABLE Vehicle (
-	licensePlate NUMBER(10) PRIMARY KEY,
+	licensePlate CHAR(6) PRIMARY KEY,
 	vehicleType CHAR(10) NOT NULL,
 	make VCHAR(100) NOT NULL,
 	model VCHAR(100) NOT NULL,
 	year NUMBER(4) NOT NULL,
 	maxPassengers NUMBER(3) NOT NULL,
-	CONSTRAINT vehicleTypeVal check(vehicleType in ('car', 'amphibious', 'bus'))
+	CONSTRAINT vehicleTypeVal check(vehicleType in ('car', 'amphibious', 'bus')),
+	CONSTRAINT yearVal check(year >= 0),
+	CONSTRAINT maxPassengersVal check(maxPassengers >= 1)
 );
 
 CREATE TABLE Tour (
@@ -30,9 +32,11 @@ CREATE TABLE Tour (
 	state VCHAR(64) NOT NULL,
 	duration NUMBER(5) NOT NULL,
 	vehicleType CHAR(10) NOT NULL,
-	adultCost NUMBER(3) NOT NULL,
-	childCost NUMBER(3) NOT NULL,
-	CONSTRAINT vehicleTypeVal check(vehicleType in ('car', 'amphibious', 'bus'))
+	adultCost NUMBER(5, 2) NOT NULL,
+	childCost NUMBER(5, 2) NOT NULL,
+	CONSTRAINT vehicleTypeVal check(vehicleType in ('car', 'amphibious', 'bus')),
+	CONSTRAINT adultCostCheck check(adultCost >= 0),
+	CONSTRAINT childCostCheck check(childCost >= 0)
 );
 
 CREATE TABLE Location (
@@ -65,6 +69,6 @@ CREATE TABLE BookedTour (
 	totalPrice NUMBER(6, 2) NOT NULL,
 	tourId NUMBER(10) FOREIGN KEY REFERENCES Tour(tourId),
 	driverLicense NUMBER(10) NOT NULL,
-	licensePlate NUMBER(10) FOREIGN KEY REFERENCES Vehicle(licensePlate),
+	licensePlate CHAR(6) FOREIGN KEY REFERENCES Vehicle(licensePlate),
 	customerId NUMBER(10) FOREIGN KEY REFERENCES Customer(customerId)
 );
