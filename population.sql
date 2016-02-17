2d71319e1db2566d8b91d0d0af2e6845d0612662
Jonathan Leitschuh <Jlleitschuh@wpi.edu>
2016-02-17 16:51:27 -0500

    Update population.sql

--- population.sql
+++ population.sql
@@ -1,3 +1,4 @@
+-- Tours!
 INSERT INTO Tour (tourName, description, city, state, duration, vehicleType, adultCost, childCost)
   VALUES ('Tour A', 'A really, really fun tour', 'Burlington', 'Vermont', 2, 'car', 39, 20);
 
@@ -15,4 +16,23 @@ INSERT INTO Tour (tourName, description, city, state, duration, vehicleType, adu
 
 INSERT INTO Tour (tourName, description, city, state, duration, vehicleType, adultCost, childCost)
   VALUES ('Tour F', 'Why are you going on this tour?', 'Forks', 'Washington', 99, 'bus', 432, 3.11);
+-- End Tours
 
+-- Locations!
+INSERT INTO Location (locationName, type, address, longitude, latitude)
+ VALUES('AppleBees', 'resturant', '100 Infinite Loop Dr.', 3949.3234, 3242.242);
+
+INSERT INTO Location (locationName, type, address, longitude, latitude)
+ VALUES('Apple Trees', 'museum', '101 Infinite Loop Dr.', 3949.32234, 3242.243242);
+
+INSERT INTO Location (locationName, type, address, longitude, latitude)
+ VALUES('Apple Keys', 'historic', '102 Infinite Loop Dr.', 3949.32124, 442.243242);
+
+INSERT INTO Location (locationName, type, address, longitude, latitude)
+ VALUES('Apple Seas', 'historic', '103 Infinite Loop Dr.', 4949.32124, 442.243242);
+ 
+ INSERT INTO Location (locationName, type, address, longitude, latitude)
+ VALUES('Apple Sneeze', 'resturant', '104 Infinite Loop Dr.', 1232.32124, 9435.243242);
+-- End Locations
+ 
+ 
