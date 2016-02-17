6492c5ade5fd9583c3e8abf8e531984d0d80a5b7
Jonathan Leitschuh <Jlleitschuh@wpi.edu>
2016-02-17 18:37:37 -0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -56,4 +56,4 @@ GROUP BY BookedTour.tourId;
 SELECT Customer.*
 FROM Customer, BookedTour
 WHERE Customer.customerId = BookedTour.customerId
-AND (YEAR(BookedTour.travelDate) = '2015' OR YEAR(BookedTour.travelDate) = '2016')
+AND (YEAR(BookedTour.travelDate) = '2015' OR YEAR(BookedTour.travelDate) = '2016');
