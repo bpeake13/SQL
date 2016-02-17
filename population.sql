f248b592a94d8224ac81b46ebad87b463647776b
Jonathan Leitschuh <Jlleitschuh@wpi.edu>
2016-02-17 16:27:00 -0500

    Create population.sql

---
+++ population.sql
@@ -0,0 +1,5 @@
+INSERT INTO Tour (tourName, description, city, state, duration, vehicleType, adultCost, childCost)
+  VALUES ('Tour A', 'A really, really fun tour', 'Burlington', 'Vermont', 2, 'car', 39, 20);
+
+INSERT INTO Tour (tourName, description, city, state, duration, vehicleType, adultCost, childCost)
+  VALUES ('Tour B', 'A really, really, really fun tour', 'Burlington', 'Massachusets', 2, 'car', 39, 20);
